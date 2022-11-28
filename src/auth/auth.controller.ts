0f453fb971bfecc75fc21f96dabc586c4a530f9c
FAMASoon <famasoon@gmail.com>
2022-11-28 12:44:06 +0000

    create login feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,23 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  NotFoundException,
+  Post,
+} from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs';
 import { RegisterDto } from './models/register.dto';
 
 @Controller()
 export class AuthController {
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {}
 
   @Post('register')
   async register(@Body() body: RegisterDto) {
     if (body.password !== body.password_confirm) {
       throw new BadRequestException('Password do not matching');
     }
-
 
     const hashed = await bcrypt.hash(body.password, 12);
 
@@ -22,5 +27,23 @@ export class AuthController {
       email: body.email,
       password: hashed,
     });
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const user = await this.userService.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    return user;
   }
 }
