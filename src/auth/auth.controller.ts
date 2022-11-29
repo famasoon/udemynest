e174234b994314cd94a7698ec54edbe11ce145fd
FAMASoon <famasoon@gmail.com>
2022-11-29 12:12:34 +0000

    logout feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -44,7 +44,7 @@ export class AuthController {
   async login(
     @Body('email') email: string,
     @Body('password') password: string,
-    @Res() response: Response,
+    @Res({ passthrough: true }) response: Response,
   ) {
     const user = await this.userService.findOne({ email });
 
@@ -67,5 +67,13 @@ export class AuthController {
     const cookie = request.cookies['jwt'];
     const data = await this.jwtService.verifyAsync(cookie);
     return this.userService.findOne({ id: data['id'] });
+  }
+
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) response: Response) {
+    response.clearCookie('jwt');
+    return {
+      message: 'success',
+    };
   }
 }
