f1a79b4cace1ac6788a06397fca2ea8722de81ec
FAMASoon <famasoon@gmail.com>
2022-12-04 11:46:05 +0000

    format

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -5,10 +5,9 @@ import { OrderService } from './order.service';
 @UseGuards(AuthGuard)
 @Controller()
 export class OrderController {
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService) {}
   @Get('orders')
   async all(@Query('page') page = 1) {
     return this.orderService.pagenate(page, ['order_items']);
   }
-
 }
