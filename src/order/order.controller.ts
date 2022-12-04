8a5046b76dcb1765970a203ec63ffd45d0e45245
FAMASoon <famasoon@gmail.com>
2022-12-04 12:17:37 +0000

    export csv

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -2,12 +2,18 @@ import {
   ClassSerializerInterceptor,
   Controller,
   Get,
+  Post,
   Query,
+  Res,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth/auth.guard';
 import { OrderService } from './order.service';
+import { Parser } from 'json2csv';
+import { Order } from './order.entity';
+import { OrderItem } from './order-item.entity';
+import { Response } from 'express';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(AuthGuard)
@@ -17,5 +23,40 @@ export class OrderController {
   @Get('orders')
   async all(@Query('page') page = 1) {
     return this.orderService.pagenate(page, ['order_items']);
+  }
+
+  @Post('export')
+  async export(@Res() res: Response) {
+    const parser = new Parser({
+      fields: ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity'],
+    });
+
+    const orders = await this.orderService.all(['order_items']);
+    const json = [];
+    orders.forEach((o: Order) => {
+      json.push({
+        ID: o.id,
+        Name: o.name,
+        Email: o.email,
+        'Product Title': '',
+        Price: '',
+        Quantity: '',
+      });
+      o.order_items.forEach((i: OrderItem) => {
+        json.push({
+          ID: '',
+          Name: '',
+          Email: '',
+          'Product Title': i.product_title,
+          Price: i.price,
+          Quantity: i.quantity,
+        });
+      });
+    });
+
+    const csv = parser.parse(json);
+    res.header('Content-Type', 'text/csv');
+    res.attachment('orders.csv');
+    return res.send(csv);
   }
 }
